fdd439a33f11ede0995730ef27d4419d773c734b
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-05-15 17:59:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/AutoFilterAdvance /Filterstorebot
+  git clone https://github.com/Coder2355/Filterstorebot /Filterstorebot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Filterstorebot
